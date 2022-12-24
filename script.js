42e04ac8c0e018da4055acb352fb7f3b6d8fc18b
Paul Macfarlane <pauljosemacfarlane@gmail.com>
2022-12-24 10:03:20 -0500

    feat: add particle rotation

--- script.js
+++ script.js
@@ -46,7 +46,43 @@ window.addEventListener('load', function (){
     }
     // eslint-disable-next-line no-unused-vars
     class Particle {
-        // TODO
+        constructor(game, x, y) {
+            this.game = game
+            this.x = x
+            this.y = y
+            this.image = document.getElementById('gears')
+            this.frameX = Math.floor(Math.random() * 3)
+            this.frameY = Math.floor(Math.random() * 3)
+            this.spriteSize = 50
+            this.sizeModifier = (Math.random() * 0.5 + 0.5).toFixed(1)
+            this.size = this.spriteSize * this.sizeModifier
+            this.speedX = Math.random() * 6 - 3
+            this.speedY = Math.random() * -15
+            this.gravity = 0.5
+            this.markedForDeletion = false
+            this.angle = 0
+            this.velecityOfAngle = Math.random() * 0.2 - 0.1
+            this.bounced = 0
+            this.bottomBounceBoundary = Math.random() * 100 + 60
+        }
+        update(){
+            this.angle += this.velecityOfAngle
+            this.speedY += this.gravity
+            this.x -= this.speedX + this.game.speed
+            this.y += this.speedY
+            if (this.y > this.game.height + this.size || this.x < 0 - this.size) this.markedForDeletion = true
+            if (this.y > this.game.height - this.bottomBounceBoundary && this.bounced < 2){
+                this.bounced++
+                this.speedY *= -0.5
+            }
+        }
+        draw(context){
+            context.save()
+            context.translate(this.x, this.y)
+            context.rotate(this.angle)
+            context.drawImage(this.image, this.frameX * this.spriteSize, this.frameY * this.spriteSize, this.spriteSize, this.spriteSize, this.size * -0.5, this.size * -0.5, this.size, this.size)
+            context.restore()
+        }
     }
     class Player {
         constructor(game) {
@@ -285,6 +321,7 @@ window.addEventListener('load', function (){
             this.ui = new UI(this)
             this.keys = []
             this.enemies = []
+            this.particles = []
             this.enemyTimer = 0
             this.enemyInterval = 1000
             this.ammo = 20
@@ -311,10 +348,15 @@ window.addEventListener('load', function (){
             } else {
                 this.ammoTimer += deltaTime
             }
+            this.particles.forEach(particle => particle.update())
+            this.particles = this.particles.filter(particle => !particle.markedForDeletion)
             this.enemies.forEach(enemy => {
                 enemy.update()
                 if (this.checkCollision(this.player, enemy)) {
                     enemy.markedForDeletion = true
+                    for (let i = 0; i < 10; i++){
+                        this.particles.push(new Particle(this, enemy.x + enemy.width * 0.5, enemy.y + enemy.height * 0.5))
+                    }
                     if (enemy.type === 'lucky') this.player.enterPowerUp()
                     else this.score--
                 }
@@ -322,7 +364,11 @@ window.addEventListener('load', function (){
                     if (this.checkCollision(projectile, enemy)) {
                         enemy.lives--
                         projectile.markedForDeletion = true
+                        this.particles.push(new Particle(this, enemy.x + enemy.width * 0.5, enemy.y + enemy.height * 0.5))
                         if (enemy.lives <= 0) {
+                            for (let i = 0; i < 10; i++){
+                                this.particles.push(new Particle(this, enemy.x + enemy.width * 0.5, enemy.y + enemy.height * 0.5))
+                            }
                             enemy.markedForDeletion = true
                             if (!this.gameOver) this.score += enemy.score
                             if (this.score > this.winningScore) this.gameOver = true
@@ -342,6 +388,7 @@ window.addEventListener('load', function (){
             this.background.draw(context)
             this.player.draw(context)
             this.ui.draw(context)
+            this.particles.forEach(particle => particle.draw(context))
             this.enemies.forEach(enemy => {
                 enemy.draw(context)
             })
